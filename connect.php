f7331aa645f686a1375b9871444f7aaa595a2b17
juanchrisniady <32811995+juanchrisniady@users.noreply.github.com>
2021-01-29 21:30:47 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$m =  new MongoDB\Client(
+    'mongodb+srv://test:test@cluster0.laqhj.mongodb.net/teknia?retryWrites=true&w=majority');
+	
+echo "Connection to database successfully";
+// select a database
+$db = $m->teknia;
+ 
+echo "Database mydb selected";
+>
